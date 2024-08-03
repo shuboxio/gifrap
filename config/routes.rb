a88e3378bfb0ba81cce4c6e31c9cb26fd71b452f
Joel Oliveira <joel.oliveira@gmail.com>
2024-08-03 17:22:01 -0400

    Add empty image_descriptions controller
    To be filled in!

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@
 Rails.application.routes.draw do
   get 'up' => 'rails/health#show', as: :rails_health_check
 
-  resources :images, only: %i[index new create edit update destroy]
+  resources :images, only: %i[index new create edit update destroy] do
+    resources :image_descriptions, only: %i[create]
+  end
 
   root 'images#index'
 end
